ec12a9256686690630e5a1306e0833ee4702dcd4
Hootsoon <hootsoon2@gmail.com>
2026-09-17 20:28:31 -0500

    Main modifications

--- ARKOV/main.cpp
+++ ARKOV/main.cpp
@@ -3,9 +3,11 @@
 #include "pid/pidController.hh"
 #include "slam/slamManager.hh"
 #include "voxelVisulization/voxel_map.hh"
-#include "utility/lidar.hh"
+#include "utility/lidar/lidar.hh"
+#include "utility/cameras/cameras.hh"
 #include "localization/particleFilter.hh"
 #include "encoded/driveOdometry.h"
+#include <unistd.h>
 
 microcontroller stm32("COM13", 115200);
 pidController leftPID(0.30, 0.05, 0.10);
@@ -17,9 +19,14 @@ driveOdometry driveOdom;
 slamManager slamMan(&pf, &driveOdom, &map, &ld);
 
 int main(){
-    slamMan.startUp();
-    for (int i = 0; i < 5; ++i) {
-        slamMan.update();
+    // slamMan.startUp();
+    // for (int i = 0; i < 5; ++i) {
+    //     slamMan.update();
+    // }
+    // return 0;
+    while(true){
+        stm32.send_Message("Test Message","non");
+        sleep(5);
     }
-    return 0;
+    
 }
